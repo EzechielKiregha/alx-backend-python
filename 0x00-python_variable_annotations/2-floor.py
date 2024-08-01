2fbbcf2fc30b4a28fb09b102a916d1b52c7ad052
EzechielKiregha <ekiregha@gmail.com>
2024-08-01 23:07:10 +0200

    floor a typed function that takes a floating point number and returns an integer

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 """
 this is a module that takes in a float
-and return its flooor
+and return its floor (rounded value)
 """
 
 
 def floor(n: float) -> int:
-    """
-    it takes in one parameter a float
+    """it takes in one parameter a float
     and returns its floor value
+
+    Args:
+        n (float): n is expected to be a float
+
+    Returns:
+        int: the integer casted value of n
     """
     return int(n)
