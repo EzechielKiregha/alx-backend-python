e18b92e84f867f628989f60b3c7887fd5ecb4ae7
EzechielKiregha <ekiregha@gmail.com>
2024-08-01 22:49:00 +0200

    Concat a typed function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    This module returns concatinated String from 2 strings
+    args
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatinate 2 strings
+
+    Args:
+        str1 (str): can only be a string
+        str2 (str): cannot be anything but a string
+
+    Returns:
+        str: the concatinated string
+    """
+    return str1 + str1
